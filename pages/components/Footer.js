023d17463ba9a48800b26397845a4894e537e9f7
Náder W. S. Sírio <nadersirio@gmail.com>
2023-05-16 16:37:18 -0300

    att-2.0

--- pages/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import Styles from "../../styles/Footer.module.css"
-
-export default function Footer() {
-  return (
-    <footer className={Styles.footer}>
-      <p><span className={Styles.footerText}>PokeNext</span> &copy; 2023</p>
-    </footer>
-  )
-}
